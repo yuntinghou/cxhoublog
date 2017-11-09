76944654132e1280b00d2b8ff86d50af9eab37d4
yuntinghou <yunting.leo@gmail.com>
2017-11-09 14:39:13 -0800

    Minor changes

--- routes/posts.js
+++ routes/posts.js
@@ -15,10 +15,16 @@ router.get('/', function(req, res, next) {
 });
 router.get('/add', function(req, res, next) {
     PostModel.create({
-        title: 'Oye, 我是神经病',
-        subtitle: '说是不是你干的？',
-        author: '王屎瓢',
-        content: '小龙人！！！'
+        title: 'Five Rams Earn ESPN Midseason All-Pro Honors',
+        subtitle: 'Rams news',
+        author: 'Kristen Lago',
+        content: 'At the season’s midpoint, five Los Angeles players have been named to ESPN’s All-Pro Team. The team is made up of a comprehensive list of 25 NFL players who have been the best of the best at their various positions through the past nine weeks.\n' +
+        '\n' +
+        'On offense, running back Todd Gurley and left tackle Andrew Whitworth were both named to the All-Pro Team. While on defense, Aaron Donald was named as the starting defensive tackle.  \n' +
+        '\n' +
+        'Gurley, who ranks second in yards from scrimmage this season with 1,024 has been dynamic in both the passing and ground game. Overall, he has recorded a league-leading seven rushing touchdowns, while also catching 29 catches for 338 yards and three touchdowns.\n' +
+        '\n' +
+        'Whitworth, widely known as one of the best offensive lineman in the league, has been paramount to Gurley’s success. Not only has he helped keep quarterback Jared Goff upright, but he has also paved the way for the Rams rushing attack. '
     }).catch(next);
     res.redirect('back')
 });
